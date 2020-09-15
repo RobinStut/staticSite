eeb89af1ef713ec50f89328fef0447174e5c1202
Robin Stut <robin@voorhoede.nl>
2020-09-15 17:57:51 +0200

    build(ssg): updating ssg

--- app/script.js
+++ app/script.js
@@ -1,32 +1,32 @@
-const mobileTrigger = document.querySelector('[data-navigation-toggle]')
-const mobileWrapper = document.querySelector('[data-navigation-mobile]')
+// const mobileTrigger = document.querySelector('[data-navigation-toggle]')
+// const mobileWrapper = document.querySelector('[data-navigation-mobile]')
 
-mobileTrigger.addEventListener("click", () => {
+// mobileTrigger.addEventListener("click", () => {
 
-  const toggle = () => mobileWrapper.classList.toggle('show')
-  const hasShowClass = Array.from(mobileWrapper.classList).some((x) => x === 'show')
-  const animateDirection = hasShowClass ? 'out' : 'in';
+//   const toggle = () => mobileWrapper.classList.toggle('show')
+//   const hasShowClass = Array.from(mobileWrapper.classList).some((x) => x === 'show')
+//   const animateDirection = hasShowClass ? 'out' : 'in';
 
-  mobileTrigger.className = 'navigation_toggle'
-  mobileTrigger.classList.add(animateDirection)
+//   mobileTrigger.className = 'navigation_toggle'
+//   mobileTrigger.classList.add(animateDirection)
 
-  document.querySelector('.one').classList.toggle('one-ani')
-  document.querySelector('.two').classList.toggle('two-ani')
+//   document.querySelector('.one').classList.toggle('one-ani')
+//   document.querySelector('.two').classList.toggle('two-ani')
 
-  if (hasShowClass) {
-    mobileWrapper.classList.add(animateDirection)
+//   if (hasShowClass) {
+//     mobileWrapper.classList.add(animateDirection)
 
-    setTimeout(() => {
-      toggle()
-      mobileWrapper.classList.remove(animateDirection)
-    }, 300);
-  }
-  else {
-    toggle()
-    mobileWrapper.classList.add(animateDirection)
-    setTimeout(() => {
-      mobileWrapper.classList.remove(animateDirection)
-    }, 300);
+//     setTimeout(() => {
+//       toggle()
+//       mobileWrapper.classList.remove(animateDirection)
+//     }, 300);
+//   }
+//   else {
+//     toggle()
+//     mobileWrapper.classList.add(animateDirection)
+//     setTimeout(() => {
+//       mobileWrapper.classList.remove(animateDirection)
+//     }, 300);
 
-  }
-});
+//   }
+// });
