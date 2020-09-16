1683fd87b7c8e322272dac71e8175acef0decd6a
Robin Stut <robin@voorhoede.nl>
2020-09-16 16:27:33 +0200

    build: init

--- app/firebase.js
+++ app/firebase.js
@@ -1,12 +1,22 @@
-const firebaseConfig = {
-    apiKey: `${process.env.apiKey}`,
-    authDomain: `${process.env.id}.firebaseapp.com`,
-    databaseURL: `https://${process.env.id}.firebaseio.com`,
-    projectId: `${process.env.id}`,
-    storageBucket: `${process.env.id}.appspot.com`,
-    messagingSenderId: `${process.env.messagingSenderId}`,
-    appId: `${process.env.appId}`
+// // Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDGOBf5W7oLezw3ZNQup2YWr480SmxG30g",
+    authDomain: "graduate-16c74.firebaseapp.com",
+    databaseURL: "https://graduate-16c74.firebaseio.com",
+    projectId: "graduate-16c74",
+    storageBucket: "graduate-16c74.appspot.com",
+    messagingSenderId: "485788019711",
+    appId: "1:485788019711:web:766682be75a5fb273b95ca"
 };
+// var firebaseConfig = {
+//   apiKey: `${process.env.apiKey}`,
+//   authDomain: `${process.env.id}.firebaseapp.com`,
+//   databaseURL: `https://${process.env.id}.firebaseio.com`,
+//   projectId: `${process.env.id}`,
+//   storageBucket: `${process.env.id}.appspot.com`,
+//   messagingSenderId: `${process.env.messagingSenderId}`,
+//   appId: `${process.env.appId}`
+// };
 // Initialize Firebase with a "default" Firebase project
 var defaultProject = firebase.initializeApp(firebaseConfig);
 
@@ -14,4 +24,4 @@ var defaultProject = firebase.initializeApp(firebaseConfig);
 var defaultDatabase = defaultProject.database();
 var auth = defaultProject.auth();
 
-console.log(defaultDatabase);
+console.log(auth);
--- app/script.js
+++ app/script.js
@@ -1,32 +1,3 @@
-// const mobileTrigger = document.querySelector('[data-navigation-toggle]')
-// const mobileWrapper = document.querySelector('[data-navigation-mobile]')
+console.log('hi Webpack');
 
-// mobileTrigger.addEventListener("click", () => {
-
-//   const toggle = () => mobileWrapper.classList.toggle('show')
-//   const hasShowClass = Array.from(mobileWrapper.classList).some((x) => x === 'show')
-//   const animateDirection = hasShowClass ? 'out' : 'in';
-
-//   mobileTrigger.className = 'navigation_toggle'
-//   mobileTrigger.classList.add(animateDirection)
-
-//   document.querySelector('.one').classList.toggle('one-ani')
-//   document.querySelector('.two').classList.toggle('two-ani')
-
-//   if (hasShowClass) {
-//     mobileWrapper.classList.add(animateDirection)
-
-//     setTimeout(() => {
-//       toggle()
-//       mobileWrapper.classList.remove(animateDirection)
-//     }, 300);
-//   }
-//   else {
-//     toggle()
-//     mobileWrapper.classList.add(animateDirection)
-//     setTimeout(() => {
-//       mobileWrapper.classList.remove(animateDirection)
-//     }, 300);
-
-//   }
-// });
+import './style.css';
