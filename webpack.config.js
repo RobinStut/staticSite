1ce202236695b62868f96c290dd94e09e9cfd0e5
Robin Stut <robin@voorhoede.nl>
2020-09-17 15:41:42 +0200

    build(webpack): setting up structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,97 +1,77 @@
-// Webpack uses this to work with directories
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-// const NunjucksWebpackPlugin = require("nunjucks-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
-
-// This is the main configuration object.
-// Here you write different options and tell Webpack what to do
 module.exports = {
-
-    // Path to your entry point. From this file Webpack will begin his work
-    entry: './app/script.js',
-
-    // Path and filename of your result bundle.
-    // Webpack will bundle all JavaScript into this file
+    entry: {
+        home: './src/pages/home/home.js',
+        login: './src/pages/login/login.js'
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
+        filename: '[name]/script.js'
     },
-
+    // https://github.com/webpack-contrib/mini-css-extract-plugin#extracting-all-css-in-a-single-file
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'styles',
+                    test: /\.css$/,
+                    chunks: 'all',
+                    enforce: true,
+                },
+            },
+        },
+    },
     module: {
         rules: [
             {
-                test: /\.js$/,
-                exclude: /(node_modules)/,
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
                 use: {
                     loader: 'babel-loader',
                     options: {
                         presets: ['@babel/preset-env']
                     }
                 }
-            },
-            {
-                // Apply rule for .sass, .scss or .css files
-                test: /\.(sa|sc|c)ss$/,
-
-                // Set loaders to transform files.
-                // Loaders are applying from right to left(!)
-                // The first loader will be applied after others
-                use: [
-                    {
-                        // After all CSS loaders we use plugin to do his work.
-                        // It gets all transformed CSS and extracts it into separate
-                        // single bundled file
-                        loader: MiniCssExtractPlugin.loader
-                    },
-                    {
-                        // This loader resolves url() and @imports inside CSS
-                        loader: "css-loader",
-                    },
-                    {
-                        // Then we apply postCSS fixes like autoprefixer and minifying
-                        loader: "postcss-loader"
-                    },
-                    {
-                        // First we transform SASS to standard CSS
-                        loader: "sass-loader",
-                        options: {
-                            implementation: require("sass")
-                        }
-                    }
-                ]
-            },
-            {
-                // Now we apply rule for images
-                test: /\.(png|jpe?g|gif|svg)$/,
-                use: [
-                    {
-                        // Using file-loader for these files
-                        loader: "file-loader",
-
-                        // In options we can set different things like format
-                        // and directory to save
-                        options: {
-                            outputPath: 'images'
-                        }
-                    }
-                ]
-            },
+            }
         ]
     },
-
     plugins: [
-
-        new MiniCssExtractPlugin({
-            filename: "bundle.css"
+        new HtmlWebpackPlugin({
+            filename: './home/index.html',
+            template: '!!ejs-webpack-loader!src/pages/home/home.ejs',
+            chunks: ['home'],
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
         }),
-
-
-    ],
-
-    // Default mode for Webpack is production.
-    // Depending on mode Webpack will apply different things
-    // on final bundle. For now we don't need production's JavaScript 
-    // minifying and other thing so let's set mode to development
-    mode: 'development'
+        new HtmlWebpackPlugin({
+            filename: './login/index.html',
+            template: '!!ejs-webpack-loader!src/pages/login/login.ejs',
+            chunks: ['login'],
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash:8].css'
+        }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorPluginOptions: {
+                preset: ['default', { discardComments: { removeAll: true } }],
+            },
+            canPrint: true
+        })
+    ]
 };
